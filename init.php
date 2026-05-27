a6fd8803b6b2b8166db3a468e9c27d4b6b0d5dbd
Андрей <andrey.buharov@gmail.com>
2026-05-28 00:25:42 +0300

    Автокоммит изменений в parsec

--- init.php
+++ init.php
@@ -1,2 +1,26 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 defined('PARSEC_VERSION') OR define('PARSEC_VERSION', '2.0.3');
+
+
+Kohana::$config->load('menu')
+    ->set('parsec', array(
+        'title' => 'Parsec',
+        'url' => '/parsec',
+        'icon' => 'fa-cog',
+        'order' => 300,
+        'children' => array(
+            'tasks' => array(
+                'title' => 'Контроль',
+                'url' => 'parsec'
+            ),
+            'setting' => array(
+                'title' => 'Настройки',
+                'url' => 'cch'
+            ),
+			'config' => array(
+                'title' => 'Конфигурация',
+                'url' => 'cch/search'
+            )
+			
+        )
+    ));
